3954037436a546b473d8104d99a0b8b26591148c
PauloM-S <paulsans@orange.fr>
2026-03-18 14:17:40 +0100

    Cleaning Com_Tel program

--- src/com_tel.py
+++ src/com_tel.py
@@ -7,12 +7,14 @@ def calculate_speed():
 
 def stop():
     print("Stopping mount...")
-    move(0,0)
+    move(0,0,1)
 
-def move(v_az, v_alt):
-    print('Correcting...')
-    if v_az > 3.5 or v_alt > 3.5:
+def move(v_az,v_alt,stopping):
+    if stopping == 0:
+        print('Correcting...')
+    if v_az > 3 or v_alt > 3:
         stop()
+        print('Error : Speed > 3')
     else:
         mount.slew_variable(nexstar.NexstarDeviceId.AZM_RA_MOTOR, v_az)
         mount.slew_variable(nexstar.NexstarDeviceId.ALT_DEC_MOTOR, v_alt)
