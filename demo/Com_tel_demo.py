a0576888604b6f25259a9b51886f37e57a438b39
PauloM-S <paulsans@orange.fr>
2026-03-18 14:14:56 +0100

    Adding Demo of the Communication with the Scope

--- demo/Com_tel_demo.py
+++ demo/Com_tel_demo.py
@@ -3,25 +3,26 @@ import time
 
 mount = nexstar.NexstarHandController('/dev/ttyUSB0')
 
-def calculate_speed():
-    print('Thinking...')
-
 def stop():
     print('Stopping mount...')
-    move(0,0)
+    move(0,0,1)
 
-def move(v_az, v_alt):
-    print('Correcting...')
-    if v_az > 3.5 or v_alt > 3.5:
+def move(v_az, v_alt,stopping):
+    if stopping == 0:
+        print('Moving...')
+    if v_az > 3 or v_alt > 3:
         stop()
+        print('Error : Speed > 3')
     else:
         mount.slew_variable(nexstar.NexstarDeviceId.AZM_RA_MOTOR, v_az)
         mount.slew_variable(nexstar.NexstarDeviceId.ALT_DEC_MOTOR, v_alt)
 
-x = input('Az and Alt speed ? (X,X with maximum 3.5)')
-x.split(',')
-az = float(x[0])
-alt = float(x[1])
-move(az,alt)
-time.sleep(2)
+x = input('Az and Alt speed ? (X.X,X.X with maximum 3)')
+az,alt = x.split(',')
+az = float(az)
+alt = float(alt)
+duration = input('Movement duration ? (XX in s)')
+duration = float(duration)
+move(az,alt,0)
+time.sleep(duration)
 stop()
