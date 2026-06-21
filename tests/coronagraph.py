6d69f04f8eee433e9488d953646a05389eeef466
paulom-s <paulsans@orange.fr>
2026-06-21 18:20:08 +0200

    Adding test file

---
+++ tests/coronagraph.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2026 Paul SANS DELISLE (@paulom-s)
+# Licensed under the MIT License.
+
+import gphoto2 as gp2
+import os
+import cv2
+import numpy as np
+import time
+
+os.system('pkill -f gvfsd-gphoto2')
+
+context = gp2.Context()
+cam = gp2.Camera()
+cam.init()
+
+def set(param, value):
+    config = cam.get_config()
+    child = config.get_child_by_name(param)
+    child.set_value(value)
+    cam.set_config(config)
+
+def capture_LV():
+    print('Downloading LiveView...')
+    preview = cam.capture_preview()
+    img = preview.get_data_and_size()
+    img = np.frombuffer(img, dtype=np.uint8)
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    return img
+
+duration = int(input('Duration ?'))
+
+for x in range(duration * 2):
+    img = capture_LV()
+    cv2.imshow('Preview', img)
+    time.sleep(0.5)
+
+cam.exit()
